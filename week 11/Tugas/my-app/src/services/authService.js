361f456efd0a77d9bcb5404fafef2bcdf44534cb
mrestuab <mrestuab54@gmail.com>
2025-06-30 15:24:13 +0700

    update

--- week 11/Tugas/my-app/src/services/authService.js
+++ week 11/Tugas/my-app/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8088";
+const API_URL = "https://backendrestu-production.up.railway.app";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
